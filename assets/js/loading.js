281278ce9e81aeb6e03d7447e23879a1d96f26bf
Catoklysm <100280242+Catoklysm@users.noreply.github.com>
2025-09-19 16:47:30 +0200

    Update loading.js

--- assets/js/loading.js
+++ assets/js/loading.js
@@ -1,43 +1,23 @@
-const social = [{squery:'discord', slink:"https://discord.gg/Yaq5HWP3h2", sname:"Discord", sicon:"discord.webp", sbackground:"discord-bg.jpg"},
-                {squery:'twitch', slink:"https://www.twitch.tv/catoklysm/", sname:"Twitch", sicon:"twitch.webp", sbackground:null},
-                {squery:'pronounspage', slink:"https://en.pronouns.page/@Catoklysm", sname:"Pronounspage", sicon:"pronounspage.webp", sbackground:null},
-                {squery:'telegram', slink:"https://t.me/catoklysm", sname:"Telegram", sicon:"telegram.webp", sbackground:null},
-                {squery:'bluesky', slink:"https://bsky.app/profile/catoklysm.bsky.social", sname:"Bluesky", sicon:"bluesky.webp", sbackground:null}];
-const url = (new URL(document.location));
-const duration = 1000;
+const social = [
+  {squery:'discord', slink:"https://discord.gg/Yaq5HWP3h2"},
+  {squery:'twitch', slink:"https://www.twitch.tv/catoklysm/"},
+  {squery:'pronounspage', slink:"https://en.pronouns.page/@Catoklysm"},
+  {squery:'telegram', slink:"https://t.me/catoklysm"},
+  {squery:'bluesky', slink:"https://bsky.app/profile/catoklysm.bsky.social"}
+];
+
+const url = new URL(document.location);
+
 window.addEventListener('load', () => {
-
-    var query = url.searchParams.get('q');
-    var succes = false;
-    if(query != null && query != ""){
-        query = query.toLowerCase();
-        social.forEach(content => {
-            if(content.squery == query){
-                const message = "Redirecting to " + content.sname + "...";
-                document.getElementById('loading-pbvalue').innerHTML = message;
-                document.title = message;
-                document.getElementById('socialicon').src = "assets/images/reficon/" + content.sicon;
-                if(content.sbackground != null){
-                    document.body.style.background = "url('assets/images/" + content.sbackground + "')"; 
-                }
-                succes = true;
-                setTimeout(function(){
-                    document.location.href = content.slink;
-                }, duration);
-            }
-        });
+  let query = url.searchParams.get('q');
+  if (query) {
+    query = query.toLowerCase();
+    const match = social.find(content => content.squery === query);
+    if (match) {
+      window.location.href = match.slink; // direct redirect
+      return;
     }
-    if(!succes){
-        document.getElementById('loading-pbvalue').innerHTML = "Invalid Query";
-        document.title = "Invalid Query";
-        setTimeout(function(){
-            document.location.href = "https://catostudios.github.io/CatoWeb/links";
-        }, duration);
-    }
+  }
+  // fallback redirect if nothing matches
+  window.location.href = "https://catostudios.github.io/CatoWeb/links";
 });
-
-
-
-
-
-
