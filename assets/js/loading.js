9220a56e9dd13b6bad8760cf36e5daad3497b6ce
Catoklysm <100280242+Catoklysm@users.noreply.github.com>
2025-09-19 15:42:27 +0200

    Update loading.js

--- assets/js/loading.js
+++ assets/js/loading.js
@@ -1,13 +1,8 @@
-const social = [{squery:'discord', slink:"https://discord.com/invite/6xdB7ZvZ95", sname:"Discord", sicon:"discord.webp", sbackground:"discord-bg.jpg"},
-                {squery:'twitch', slink:"https://www.twitch.tv/catovr/", sname:"Twitch", sicon:"twitch.webp", sbackground:null},
-                {squery:'tiktok', slink:"https://www.tiktok.com/@catovrc", sname:"TikTok", sicon:"tiktok.webp", sbackground:null},
-                {squery:'instagram', slink:"https://www.instagram.com/furry_cato/", sname:"Instagram", sicon:"instagram.webp", sbackground:null},
-                {squery:'melody', slink:"https://melodybot.ga/", sname:"Melody Bot", sicon:"melody.webp", sbackground:null},
-                {squery:'donation', slink:"https://streamlabs.com/catovr/tip", sname:"Streamlabs", sicon:"streamlabs.webp", sbackground:null},
-                {squery:'commissions', slink:"https://catovr.gay/commissions", sname:"Commissions", sicon:"commissions.webp", sbackground:null},
+const social = [{squery:'discord', slink:"https://discord.gg/Yaq5HWP3h2", sname:"Discord", sicon:"discord.webp", sbackground:"discord-bg.jpg"},
+                {squery:'twitch', slink:"https://www.twitch.tv/catoklysm/", sname:"Twitch", sicon:"twitch.webp", sbackground:null},
                 {squery:'pronounspage', slink:"https://pronouns.page/@CatoVR", sname:"Pronounspage", sicon:"pronounspage.webp", sbackground:null},
-                {squery:'pettingtown', slink:"https://petting.town/cato", sname:"Pettingtown", sicon:"pettingtown.webp", sbackground:null},
-                {squery:'twitter', slink:"https://twitter.com/CatoVR", sname:"Twitter", sicon:"twitter.webp", sbackground:null}];
+                {squery:'Telegram', slink:"https://t.me/catoklysm", sname:"Telegram", sicon:"Telegram.webp", sbackground:null},
+                {squery:'bluesky', slink:"https://bsky.app/profile/catoklysm.bsky.social", sname:"Bluesky", sicon:"bluesky.webp", sbackground:null}];
 const url = (new URL(document.location));
 const duration = 1500;
 window.addEventListener('load', () => {
@@ -40,4 +35,5 @@ window.addEventListener('load', () => {
         }, duration);
     }
 });
+
 
